3d85ee87fe2ac973dde94eb2ba475985db3fea54
1760842192@13.com <17608421962@163.com>
2021-01-30 21:51:28 +0800

    使用 menu 菜单，让menu 菜单自动依付在Tool bar 上面

--- app/src/main/java/com/wyp/wxplayer/activity/MainActivity.java
+++ app/src/main/java/com/wyp/wxplayer/activity/MainActivity.java
@@ -1,8 +1,11 @@
 package com.wyp.wxplayer.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.wyp.wxplayer.R;
 
@@ -22,5 +25,30 @@ public class MainActivity extends AppCompatActivity {
 
         // 将 ToolBar 设置为标题栏
         setSupportActionBar(mToolbar);
+    }
+
+    @Override
+    /*
+    Android一共有三种形式的菜单:
+    1.选项菜单（optinosMenu）
+    2.上下文菜单（ContextMenu）
+    3.子菜单(subMenu)
+    其中最常用的就是选项菜单(optionsMenu), 该菜单在点击 menu 按键 后会在对应的Activity底部显示出来。
+    * */
+    public boolean onCreateOptionsMenu(Menu menu) {//选项菜单
+        // 创建 menu 菜单，这个菜单会依附到 ToolBar 上，ToolBar的特性
+        getMenuInflater().inflate(R.menu.activity_main,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {// 处理 menu 菜单的点击监听
+        switch (item.getItemId()){
+            case R.id.menu_main_settings:
+                Intent intent = new Intent(this,WXSetting_Activity.class);
+                startActivity(intent);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
---
+++ app/src/main/java/com/wyp/wxplayer/activity/WXSetting_Activity.java
@@ -0,0 +1,15 @@
+package com.wyp.wxplayer.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.wyp.wxplayer.R;
+
+public class WXSetting_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wxsetting);
+    }
+}
