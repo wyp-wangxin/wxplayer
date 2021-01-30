fd4e44d629f34b71e36de64661ac09c5a14834c9
1760842192@13.com <17608421962@163.com>
2021-01-30 20:07:03 +0800

    1、将系统里面原本的 标题栏隐藏 2、在mainactivity 里面设置我们自己标题栏

--- app/src/main/java/com/wyp/wxplayer/activity/MainActivity.java
+++ app/src/main/java/com/wyp/wxplayer/activity/MainActivity.java
@@ -1,15 +1,26 @@
 package com.wyp.wxplayer.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import com.wyp.wxplayer.R;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.toolbar)
+    Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        // 将 ToolBar 设置为标题栏
+        setSupportActionBar(mToolbar);
     }
 }
