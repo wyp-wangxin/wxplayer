18ed462b30897484aa909f0c0eea2e09fc068d03
1760842192@13.com <17608421962@163.com>
2021-01-31 22:18:04 +0800

    1、初始化 recyclerview 布局，使用列表就行了

--- app/src/main/java/com/wyp/wxplayer/fragment/homepage/HomeFragment.java
+++ app/src/main/java/com/wyp/wxplayer/fragment/homepage/HomeFragment.java
@@ -1,6 +1,7 @@
 package com.wyp.wxplayer.fragment.homepage;
 
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -34,13 +35,17 @@ public class HomeFragment extends BaseFragment implements HomeMvp.View {
 
     @Override
     protected void initVeiw() {
-        TextView textView = mRootview.findViewById(R.id.tv_test);
-
-        textView.setText("这是home fragment");
-
         // 创建 presenter
         mPresenter = new HomePresenter(this);
         mPresenter.loadData(0, 10);
+
+        // 填充 RecylerView 列表。 RecylerView,的基础用法
+        // 布局管理器
+        LinearLayoutManager layout = new LinearLayoutManager(getContext());//RecylerView 列表
+//        GridLayoutManager layout = new GridLayoutManager(getContext(),2);// RecylerView 九宫格
+//        StaggeredGridLayoutManager layout = new StaggeredGridLayoutManager(2,LinearLayoutManager.VERTICAL);// RecylerView 瀑布流
+        layout.setOrientation(LinearLayoutManager.VERTICAL);
+        mRecylerview.setLayoutManager(layout);
     }
 
     @Override
