87948e864824b8643e48487965ee9c14a49e1799
1760842192@13.com <17608421962@163.com>
2021-02-02 20:54:07 +0800

    1、添加 'com.github.bumptech.glide:glide:3.7.0 用于laod 图片，加载到控件 2、注释掉一些语句，因为现在没有真正的资源文件

--- app/src/main/java/com/wyp/wxplayer/adapter/HomeAdapter.java
+++ app/src/main/java/com/wyp/wxplayer/adapter/HomeAdapter.java
@@ -1,12 +1,15 @@
 package com.wyp.wxplayer.adapter;
 
+import android.graphics.Point;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.wyp.wxplayer.R;
+import com.wyp.wxplayer.Util;
 import com.wyp.wxplayer.bean.VideoBean;
 import com.wyp.wxplayer.utils.myLog;
 
@@ -42,17 +45,22 @@ public class HomeAdapter extends RecyclerView.Adapter<HomeAdapter.MyViewHolder> 
      * 为 ViewHolder 绑定数据
      */
     public void onBindViewHolder(MyViewHolder holder, int position) {
-        // 获取到当前条目的数据
-        VideoBean videoBean = mVideoBeans.get(position);
+        // 获取到当前条目的数据 我们没有真正的资源
+        //VideoBean videoBean = mVideoBeans.get(position);
 
-        // 填充内容
-        holder.mTvTitle.setText(videoBean.getTitle());
-        holder.mTvDescription.setText(videoBean.getDescription());
+        // 填充内容 我们没有真正的资源
+        //holder.mTvTitle.setText(videoBean.getTitle());
+        //holder.mTvDescription.setText(videoBean.getDescription());
+        holder.mTvTitle.setText("《火影忍者》");
+        holder.mTvDescription.setText("好看的日漫更新了！");
+        // 加载图片 我们没有真正的资源
+        //Glide.with(holder.itemView.getContext()).load(videoBean.getPosterPic()).into(holder.mIvContentimg);
     }
 
     @Override
     public int getItemCount() {
-        return mVideoBeans.size();
+       // return mVideoBeans.size();
+        return 10; //我们没有真正的资源
     }
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
@@ -66,10 +74,22 @@ public class HomeAdapter extends RecyclerView.Adapter<HomeAdapter.MyViewHolder> 
         TextView mTvTitle;
         @Bind(R.id.tv_description)
         TextView mTvDescription;
+        @Bind(R.id.viewbg)
+        View viewbg;
 
         public MyViewHolder(View itemView) {
             super(itemView);
             ButterKnife.bind(this, itemView);
+
+            // 初始化图片大小
+            Point point = Util.computeImgSize(640,540,itemView.getContext());
+            mIvContentimg.getLayoutParams().width = point.x;
+            mIvContentimg.getLayoutParams().height = point.y;
+            mIvContentimg.requestLayout();
+            //给布局一个灰色，让文字看的清楚
+            viewbg.getLayoutParams().width = point.x;
+            viewbg.getLayoutParams().height = point.y;
+            viewbg.requestLayout();
         }
     }
 
